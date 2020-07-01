258b4ca7b3b33f061779f87f540fc55d21530f39
Pietra Carozzo <pietra_carozzo@carrefour.com>
2020-06-30 21:55:46 -0300

    Relembrei como fazer um código básico de Console.Net pelo Visual Studio. Arquivo feito especialmente para acompanhar as aulas de Backend Developer do Carrefour.

---
+++ Relembrando_teste/Relembrando_teste/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Relembrando_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, idade, cidade;
+            Console.WriteLine("Oi pessoa! Hoje estamos aqui pra pegar alguns dados seus, beleza? \n");
+            Console.WriteLine("Me diz aí, como você chama? ");
+            nome = Console.ReadLine();
+            Console.WriteLine("\nE quantos anos você tem? ");
+            idade = Console.ReadLine();
+            Console.WriteLine("\nTá acabando já, só preciso saber agora onde você mora?");
+            cidade = Console.ReadLine();
+            Console.WriteLine("\nConfere aí pra mim meu parceiro... \nNome: " + nome + "\nIdade: " + idade + "\nCidade: " + cidade);
+            Console.ReadLine();
+            Console.WriteLine("\nÉ só isso mesmo, se tiver tudo certinho a gente finaliza por aqui. Até mais!");
+            Console.ReadLine();
+        }
+    }
+}
